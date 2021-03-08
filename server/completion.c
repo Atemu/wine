51d8d7294e12c93c791517d0a7b5023ec46e448d
Elizabeth Figura <zfigura@codeweavers.com>
2021-03-08 14:31:06 -0600

    server: Add an object operation to retrieve a fast synchronization object.
    (cherry picked from commit 68754fe170b9c880619f2e036916f78791bb269d)

--- server/completion.c
+++ server/completion.c
@@ -193,6 +193,7 @@ static const struct object_ops completion_ops =
     default_unlink_name,       /* unlink_name */
     no_open_file,              /* open_file */
     no_kernel_obj_list,        /* get_kernel_obj_list */
+    no_get_fast_sync,          /* get_fast_sync */
     completion_close_handle,   /* close_handle */
     completion_destroy         /* destroy */
 };
